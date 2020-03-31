6338e9f7a379d0d4208f7f2e07b913ea7df1af9b
Mad Hallé <mad.halle@icloud.com>
2020-03-31 10:12:53 -0600

    iteration 3 tests written

--- test/building_test.rb
+++ test/building_test.rb
@@ -9,7 +9,9 @@ class BuildingTest < MiniTest::Test
   def setup
     @building = Building.new
     @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 1, bedrooms: 2})
+    @unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    @unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
   end
 
   def test_it_exists
@@ -52,5 +54,53 @@ class BuildingTest < MiniTest::Test
     @building.add_unit(@unit2)
     assert_equal 1099.5, @building.average_rent
   end
+
+  def test_rented_units
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @building.add_unit(@unit3)
+    assert_equal [], @building.rented_units
+
+    @unit2.add_renter(@renter1)
+    assert_equal [@unit2], @building.rented_units
+  end
+
+  def test_renter_with_highest_rent
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @building.add_unit(@unit3)
+
+    @unit2.add_renter(@renter1)
+    assert_equal @renter1, @building.renter_with_highest_rent
+
+    @unit1.add_renter(@renter2)
+    assert_equal @renter2, @building.renter_with_highest_rent
+
+    renter3 = Renter.new("Max")
+    @unit3.add_renter(renter3)
+    assert_equal @renter2, @building.renter_with_highest_rent
+  end
+
+  def test_building_units_by_number_of_bedrooms
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @building.add_unit(@unit3)
+    @building.add_unit(@unit4)
+    assert_equal [], @building.units_by_number_of_bedrooms
+  end
+
+
+
+  #
+  # pry(main)> building.add_unit(unit4)
+  #
+  # pry(main)> building.units_by_number_of_bedrooms
+  # # =>
+  # # {
+  # #   3 => ["D4" ],
+  # #   2 => ["B2", "C3"],
+  # #   1 => ["A1"]
+  # # }
+  # ```
 
 end
