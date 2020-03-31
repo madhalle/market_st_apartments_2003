811d528b1a6bffcba9787ae8e64562b5ed879665
Mad Hallé <mad.halle@icloud.com>
2020-03-31 09:23:16 -0600

    working apartment class, passes all tests

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,10 +1,15 @@
 class Apartment
-  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
   def initialize(args)
     @number = args[:number]
     @monthly_rent = args[:monthly_rent]
     @bathrooms = args[:bathrooms]
     @bedrooms = args[:bedrooms]
+    @renter = nil
   end
 
+
+  def add_renter(renter)
+    @renter = renter
+  end
 end
