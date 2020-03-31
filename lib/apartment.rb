3a91f1c9b2b7d45666ceacbd9b925a2f29304d9d
Mad Hallé <mad.halle@icloud.com>
2020-03-31 09:18:47 -0600

    initialize apartment class

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,4 +1,10 @@
 class Apartment
-
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms
+  def initialize(args)
+    @number = args[:number]
+    @monthly_rent = args[:monthly_rent]
+    @bathrooms = args[:bathrooms]
+    @bedrooms = args[:bedrooms]
+  end
 
 end
